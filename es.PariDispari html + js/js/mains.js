d134041d5ac0543892f85bf21bf90cc74ecba13e
valeria caramellino <valeriacaramellino@gmail.com>
2023-04-24 11:47:19 +0200

    rifatto es

--- es.PariDispari html + js/js/mains.js
+++ es.PariDispari html + js/js/mains.js
@@ -5,47 +5,73 @@
 // Sommiamo i due numeri e dichiariamo chi ha
 // vinto (controlliamo se la somma da pari o dispari).
 
-//chiedo all'utente di scegliere pari o dispari
-let sceltaUtentePD =  prompt("Scrivi pari o dispari!");
-console.log(sceltaUtentePD);
-//chiedo all'utente un numero tra 0 e 5
-let sceltaNumUtnte= parseInt(prompt("Scegli un numero tra 0 e 5"));
-console.log(sceltaNumUtnte);
+// creo prompt per memorizzare i dati inseriti dall'utente(scelta pari/dispari)
+let sceltaUtente = prompt("Ciao, scegli pari o dispari!");
+sceltaUtente = sceltaUtente.toLowerCase();
+//.toLowerCase restituisce l'esatto contenuto della stringa nel nostro caso le parole pari dispari 
+let sceltaPDUtente;
+
+// creo condizione in modo che l'utente inserisca i dati in maniera corretta
+if (sceltaUtente == "pari") {
+    sceltaPDUtente = true;
+} else if(sceltaUtente == "dispari") {
+    sceltaPDUtente = false;
+} else {
+    sceltaPDUtente = null;
+}
+
+//creo un ciclo while in modo che l'utente possa scegliere solo pari e dispari
+while (sceltaPDUtente == null ) {
+    alert("La tua scelta non comprende pari o dispari!");
+    sceltaUtente = prompt("Ciao, scegli pari o dispari");
+    sceltaUtente = sceltaUtente.toLowerCase();
+    if (sceltaUtente == "pari") {
+        sceltaPDUtente = true;
+    } else if(sceltaUtente == "dispari") {
+        sceltaPDUtente = false;
+    } else {
+        sceltaPDUtente = null;
+    }
+}
+
+// creo prompt per memorizzare il num scelto e uso parseInt per trasformare la stringa in numero
+let numeroUtente = parseInt(prompt("Scegli un numero da 1 a 5"));
+
+// uso isNan nel caso l'utente non abbia inserito un numero
+while (isNaN(numeroUtente) || (numeroUtente < 1) || (numeroUtente > 5)) {
+    alert("Non hai inserito un numero o non è un numero compreso tra 1 e 5!");
+    numeroUtente = parseInt(prompt("Scegli un numero da 1 a 5"));        
+}
+
+// creo le variabili con numero casuale
+let numeroPC = getRandomNumber();
+alert("Il numumero che ha scelto il pc è "+numeroPC);
 
 
-//genero numero random per pc
-function numeroRandom (min, max){
+let somma = (numeroUtente + numeroPC);
+alert("La somma dei due numeri è "+somma);
 
-    //creo variabile generica
-    let numero;
-    //indico il valore della variabile
-    numero = Math.floor( Math.random() * (max - min + 1) + min );
-    //restituisco la variabile
+// creo funzione numero casuale compreso tra 1 e 5
+function getRandomNumber() {  
+    let numero = Math.floor((Math.random() * 5) + 1);
     return numero;
 }
-//indico il valore che voglio e le assegno una variabile
-let risultatoPc= numeroRandom(0,5);
-console.log(risultatoPc);
 
-//sommo i valori risultatoPc + sceltaNumUtente e creo una variabile
-
-let sommaValori= sceltaNumUtnte + risultatoPc ;
-console.log(sommaValori);
-
-
-//confrontiamo se la somma è pari o dispari
-let risultatoPD;
-if(sommaValori %2 ==0){
-    risultatoPD= " pari";
-    console.log("la somma è pari");
-}else{
-    risultatoPD=" dispari";
-    console.log("la somma è dispari");
+// creo funzione che decide se la somma dei due numeri è pari e dispari
+function pariDispari(sommaNumeri) {
+    if (sommaNumeri % 2 == 0) {
+        return true
+    } 
+    return false
 }
-let valoreFinalePD= risultatoPD;
-console.log(valoreFinalePD);
 
-//confronto la scelta iniziale dell'utente tra pari e dispari
-//con il risultato finale che definisce il valore
-//in base a questo si vince o si perde
+
+let risultatoFinale = pariDispari(somma);
+
+// creo condizione per informare il player del risultato
+if (risultatoFinale == sceltaPDUtente) {
+    alert("Complimenti, hai vinto!");
+} else {
+    alert("Mi dispiace, hai perso!");
+}
 
